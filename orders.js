80fb3483a56a596a7998f691c9de224903b22061
ameena117 <ameenajadallah@gmail.com>
2024-02-09 16:48:37 +0200

    add sub

--- orders.js
+++ orders.js
@@ -10,16 +10,12 @@ const menu = document.querySelector(".menu");
 let order = document.querySelector(".orederSummaryView .orderSummary");
 let total = document.querySelector(".orederSummaryView .totalPrice");
 let orderItems = [];
-let count = 0;
 let itemSelected = [];
 let totalPrice = 0;
+let count =0;
 menu.innerHTML = items
   .map(
-    ({
-      item,
-      price,
-      img,
-    }) => `
+    ({ item, price, img }) => `
     <div class="mainItem">
     <div class="item">
     ${item} <img src="${img}">
@@ -30,6 +26,9 @@ menu.innerHTML = items
   )
   .join("");
 let item = document.querySelectorAll(".mainItem");
+let addBtn = [];
+let subBtn = [];
+
 for (let i = 0; i < item.length; i++) {
   item[i].addEventListener("click", function (e) {
     itemSelected.push(i);
@@ -38,6 +37,7 @@ for (let i = 0; i < item.length; i++) {
     count = itemSelected.filter(function (value) {
       return value === i;
     }).length;
+    console.log(count);
     if (count <= 1) {
       orderItems.push({
         item: items[i].item,
@@ -52,16 +52,63 @@ for (let i = 0; i < item.length; i++) {
       orderItems[index].count = count;
       orderItems[index].total = items[i].price * count;
     }
-    console.log(orderItems);
-
     order.innerHTML = orderItems
       .map(
         ({ item, price, count, total }) =>
-          `<div class="orderItem">#${count} ${item}: $${price} = $${total}</div>`
+          `<div class="orderItem">#<span class="count" >${count}</span> ${item}: $${price} = $<span class="total" >${total}</span> <button class="addBtn">+</button> <button class="subBtn">-</button></div>`
       )
       .join("");
     totalPrice = orderItems.reduce((a, b) => a + b.total, 0);
     console.log(totalPrice);
     total.innerHTML = `<hr/>  Total Price = $${totalPrice}`;
+    addBtn = document.querySelectorAll(".orderItem .addBtn");
+    subBtn = document.querySelectorAll(".orderItem .subBtn");
+    for (let i = 0; i < addBtn.length; i++) {
+      addBtn[i].onclick = function (e) {
+        orderItems[i].count++;
+        orderItems[i].total = orderItems[i].price * orderItems[i].count;
+        let orderSelectedCount = document.querySelectorAll(".orderItem .count");
+        let orderSelectedTotal = document.querySelectorAll(".orderItem .total");
+        orderSelectedCount[i].innerHTML = orderItems[i].count;
+        orderSelectedTotal[i].innerHTML = orderItems[i].total;
+        totalPrice = orderItems.reduce((a, b) => a + b.total, 0);
+        total.innerHTML = `<hr/>  Total Price = $${totalPrice}`;
+      };
+    }
+    for (let i = 0; i < subBtn.length; i++) {
+      subBtn[i].onclick = function (e) {
+        console.log(0);
+
+        orderItems[i].count--;
+        orderItems[i].total = orderItems[i].price * orderItems[i].count;
+        console.log(orderItems[i]);
+
+        let orderSelectedCount = document.querySelectorAll(".orderItem .count");
+        let orderSelectedTotal = document.querySelectorAll(".orderItem .total");
+        console.log(orderItems);
+        console.log(orderSelectedCount);
+        console.log(orderSelectedTotal);
+        orderSelectedCount[i].innerHTML = orderItems[i].count;
+        orderSelectedTotal[i].innerHTML = orderItems[i].total;
+        totalPrice = orderItems.reduce((a, b) => a + b.total, 0);
+        total.innerHTML = `<hr/>  Total Price = $${totalPrice}`;
+        if (orderItems[i].count < 1) {
+          orderItems.splice(i, 1);
+          console.log(orderItems);
+          order.innerHTML = orderItems
+            .map(
+              ({ item, price, count, total }) =>
+                `<div class="orderItem">#<span class="count" >${count}</span> ${item}: $${price} = $<span class="total" >${total}</span> <button class="addBtn">+</button><button class="subBtn">-</button></div>`
+            )
+            .join("");
+          totalPrice = orderItems.reduce((a, b) => a + b.total, 0);
+          
+          console.log(totalPrice);
+          totalPrice == 0
+            ? (total.innerHTML = "")
+            : (total.innerHTML = `<hr/>  Total Price = $${totalPrice}`);
+        }
+      };
+    }
   });
 }
