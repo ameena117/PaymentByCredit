f64c6f123ecbfaccdc563824b98836309d516f0e
ameena117 <ameenajadallah@gmail.com>
2024-02-10 09:22:43 +0200

    functions

--- orders.js
+++ orders.js
@@ -28,17 +28,51 @@ menu.innerHTML = items
 let item = document.querySelectorAll(".mainItem");
 let addBtn = [];
 let subBtn = [];
+const cart = document.querySelector(".cart");
+function addToOrder() {
+  order.innerHTML = orderItems
+    .map(
+      ({ item, price, count, total }) =>
+        `<div class="orderItem">#<span class="count" >${count}</span> ${item}: $${price} = $<span class="total" >${total}</span> <button class="addBtn">+</button> <button class="subBtn">-</button></div>`
+    )
+    .join("");
+  totalPrice = orderItems.reduce((a, b) => a + b.total, 0);
+  total.innerHTML = `<hr/>  Total Price = $${totalPrice}`;
+  let totalCount = orderItems.reduce((a, b) => a + b.count, 0);
+  cart.innerHTML = totalCount;
+}
+function updateOrder(i, orderSelectedCount, orderSelectedTotal) {
+  console.log(i);
+  orderSelectedCount[i].innerHTML = orderItems[i].count;
+  orderSelectedTotal[i].innerHTML = orderItems[i].total;
+  totalPrice = orderItems.reduce((a, b) => a + b.total, 0);
+  total.innerHTML = `<hr/>  Total Price = $${totalPrice}`;
+  if (orderItems[i].count < 1) {
+    orderItems.splice(i, 1);
+    console.log(orderItems);
+    order.innerHTML = orderItems
+      .map(
+        ({ item, price, count, total }) =>
+          `<div class="orderItem">#<span class="count" >${count}</span> ${item}: $${price} = $<span class="total" >${total}</span> <button class="addBtn">+</button><button class="subBtn">-</button></div>`
+      )
+      .join("");
+    totalPrice = orderItems.reduce((a, b) => a + b.total, 0);
+    itemSelected = orderItems.map((item) => i);
+    console.log(itemSelected);
+    totalPrice == 0
+      ? (total.innerHTML = "")
+      : (total.innerHTML = `<hr/>  Total Price = $${totalPrice}`);
+  }
+}
 
 for (let i = 0; i < item.length; i++) {
   item[i].addEventListener("click", function (e) {
     itemSelected.push(i);
-    console.log(itemSelected);
-    console.log(items[i].item);
-    console.log(items[i].price);
     count = itemSelected.filter(function (value) {
       return value === i;
     }).length;
     console.log(count);
+    console.log(itemSelected);
     if (count <= 1) {
       orderItems.push({
         item: items[i].item,
@@ -46,74 +80,35 @@ for (let i = 0; i < item.length; i++) {
         count: count,
         total: items[i].price * count,
       });
-    } else {
+      console.log("new");
+    } else if(count > 1) {
       const index = orderItems.findIndex((obj) => {
+        console.log("exiting");
         return obj.item === items[i].item;
       });
       orderItems[index].count = count;
       orderItems[index].total = items[i].price * count;
     }
-    order.innerHTML = orderItems
-      .map(
-        ({ item, price, count, total }) =>
-          `<div class="orderItem">#<span class="count" >${count}</span> ${item}: $${price} = $<span class="total" >${total}</span> <button class="addBtn">+</button> <button class="subBtn">-</button></div>`
-      )
-      .join("");
-    totalPrice = orderItems.reduce((a, b) => a + b.total, 0);
-    console.log(totalPrice);
-    total.innerHTML = `<hr/>  Total Price = $${totalPrice}`;
-    let totalCount = orderItems.reduce((a, b) => a + b.count, 0);
-    console.log(totalCount);
-    document.querySelector(".cart").innerHTML = totalCount;
+    addToOrder();
     addBtn = document.querySelectorAll(".orderItem .addBtn");
     subBtn = document.querySelectorAll(".orderItem .subBtn");
+
     for (let i = 0; i < addBtn.length; i++) {
       addBtn[i].onclick = function (e) {
         orderItems[i].count++;
         orderItems[i].total = orderItems[i].price * orderItems[i].count;
         let orderSelectedCount = document.querySelectorAll(".orderItem .count");
         let orderSelectedTotal = document.querySelectorAll(".orderItem .total");
-        orderSelectedCount[i].innerHTML = orderItems[i].count;
-        orderSelectedTotal[i].innerHTML = orderItems[i].total;
-        totalPrice = orderItems.reduce((a, b) => a + b.total, 0);
-        total.innerHTML = `<hr/>  Total Price = $${totalPrice}`;
+        updateOrder(i, orderSelectedCount, orderSelectedTotal);
       };
     }
     for (let i = 0; i < subBtn.length; i++) {
       subBtn[i].onclick = function (e) {
-        console.log(0);
-
         orderItems[i].count--;
         orderItems[i].total = orderItems[i].price * orderItems[i].count;
-        console.log(orderItems[i]);
-
         let orderSelectedCount = document.querySelectorAll(".orderItem .count");
         let orderSelectedTotal = document.querySelectorAll(".orderItem .total");
-        console.log(orderItems);
-        console.log(orderSelectedCount);
-        console.log(orderSelectedTotal);
-        orderSelectedCount[i].innerHTML = orderItems[i].count;
-        orderSelectedTotal[i].innerHTML = orderItems[i].total;
-        totalPrice = orderItems.reduce((a, b) => a + b.total, 0);
-        total.innerHTML = `<hr/>  Total Price = $${totalPrice}`;
-        if (orderItems[i].count < 1) {
-          orderItems.splice(i, 1);
-          console.log(orderItems);
-          order.innerHTML = orderItems
-            .map(
-              ({ item, price, count, total }) =>
-                `<div class="orderItem">#<span class="count" >${count}</span> ${item}: $${price} = $<span class="total" >${total}</span> <button class="addBtn">+</button><button class="subBtn">-</button></div>`
-            )
-            .join("");
-          totalPrice = orderItems.reduce((a, b) => a + b.total, 0);
-          console.log(itemSelected);
-          itemSelected.splice(i, 1);
-          console.log(itemSelected);
-          console.log(totalPrice);
-          totalPrice == 0
-            ? (total.innerHTML = "")
-            : (total.innerHTML = `<hr/>  Total Price = $${totalPrice}`);
-        }
+        updateOrder(i, orderSelectedCount, orderSelectedTotal);
       };
     }
   });
